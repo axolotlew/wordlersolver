aecc2da231b8271aa62404f913e13e3d786eb701
axolotlew <axolotlew@gmail.com>
2025-08-06 03:01:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,44 @@
-// Name for the cache
-const CACHE_NAME = 'wordle-cache-v1';
+const CACHE_NAME = 'wordle-cache-v3';
 
-// Files to cache for offline use
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/solver.js',
-  '/RussianFreq.txt',
-  '/RussianJunk.txt',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',                     // resolves to repo root
+  './index.html',
+  './manifest.json',
+  './solver.js',
+  './RussianFreq.txt',
+  './RussianJunk.txt',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Install Service Worker and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('[ServiceWorker] Caching app shell and content');
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate Service Worker and remove old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        })
-      )
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
   self.clients.claim();
 });
 
-// Fetch from cache, fall back to network
 self.addEventListener('fetch', event => {
+  // Force all requests into repo subfolder
+  let requestURL = new URL(event.request.url);
+  if (requestURL.origin === location.origin && requestURL.pathname.startsWith('/')) {
+    if (!requestURL.pathname.startsWith('/wordlersolver/')) {
+      requestURL.pathname = '/wordlersolver' + requestURL.pathname;
+      event.respondWith(fetch(requestURL.toString()));
+      return;
+    }
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
